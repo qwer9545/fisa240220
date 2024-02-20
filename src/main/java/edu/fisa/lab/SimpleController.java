25e040f95495c6b1f2f35daf76481160ec18de95
qwer9545 <rkddnjs723@gmail.com>
2024-02-20 11:17:33 +0900

    update 333

---
+++ src/main/java/edu/fisa/lab/SimpleController.java
@@ -0,0 +1,16 @@
+package edu.fisa.lab;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class SimpleController {
+	
+	@GetMapping("getdata")
+	public String get() {
+		System.out.println("clear");
+		return "fisa data";
+	}
+	
+}
